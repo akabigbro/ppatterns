bc7cc8bf01221917f9b3881c04bac5af5dbd7b23
akabigbro <akabigbro@1374c9c0-61e3-11de-9151-f7b1c15b93db>
2010-03-14 16:14:32 +0000

    now when you set state, your observers are notified automatically

--- behavioral/stateful.cxx
+++ behavioral/stateful.cxx
@@ -1,10 +1,5 @@
 #include "stateful.h"
 
-void Stateful::setState(int state)
-{
-    this->state = state;
-}
-
 Stateful::Stateful(int state)
     : state(state)
 {
@@ -13,4 +8,10 @@ Stateful::Stateful(int state)
 int Stateful::getState(void)
 {
     return state;
+}
+
+void Stateful::setState(int state)
+{
+    this->state = state;
+    Notify();
 }
